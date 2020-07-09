dc98192f7b8c883b6b45d1ad8900c7ea5d06d0b9
veli4kodima <veli4kodima@gmail.com>
2020-07-09 10:42:41 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $('.feed_slider').slick({
+        centerMode: true,
+        centerPadding: '00px',
+        slidesToShow: 3,
+        speed:1000,
+        easing: 'ease',
+        centerMode:true,
+      });
+  });
